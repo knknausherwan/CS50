a718a02874893b9a9d115f4b0f1323498168acc2
knknausherwan <knknausherwan@users.noreply.github.com>
2019-09-03 08:44:30 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -5,51 +5,25 @@ int main(void)
 {
     int n;
     int i;
+    // User Input limited from 0-9
     do
     {
         n = get_int("Height: ");
     }
     while (n < 0 || n > 9);
-    
-    for (i = 0; i < n; i++)
+    //Loop for Pyramid
+    for (i = 0; i <= n; i++)
     {
-        if (i == 0)
-        {
-            printf("        ");
-        }
-        if (i == 1)
-        {
-            printf("       ");
-        }
-        if (i == 2)
-        {
-            printf("      ");
-        }
-        if (i == 3)
-        {
-            printf("     ");
-        }
-        if (i == 4)
-        {
-            printf("    ");
-        }
-        if (i == 5)
-        {
-            printf("   ");
-        }
-        if (i == 6)
-        {
-            printf("  ");
-        }
-        if (i == 7)
+        //Loop for Spacing
+        for (size_t k = n; k > i; k--)
         {
             printf(" ");
         }
+        //Loop for hashes
         for (size_t j = 0; j<= i; ++j)
         {
             printf("#");
         }
-        printf(" ");
         printf("\n");
     }
 }
